778e9d1d8c5f76c824406e941eeac8c0b01d5156
Jake Rule <rule@OXASWELAP13.physics.ox.ac.uk>
2025-02-26 17:32:59 -0500

    Check if cooling func used at all

--- src/pgen/gr_torus_cooling_loops.cpp
+++ src/pgen/gr_torus_cooling_loops.cpp
@@ -1893,7 +1893,9 @@ void Cooling(Mesh *pm, const Real dt) {
     int nmb = pmbp->nmb_thispack;
     par_for("User_Source_Cooling", DevExeSpace(), 0,nmb-1,ks,ke+1,js,je+1,is,ie+1,
     KOKKOS_LAMBDA(int m, int k, int j, int i) {
-      
+      if ((k==0)&&(j==0)&&(i==0)){
+        printf("CoolingFuncCalled Starg=%f \n",s_targ);
+      }
       int km1 = (k-1 < ks) ? ks : k-1;
       int kp1 = (k+1 > ke) ? ke : k+1;
       int jm1 = (j-1 < js) ? js : j-1;
@@ -1994,7 +1996,7 @@ void Cooling(Mesh *pm, const Real dt) {
         // u0_(m,IM3,k,j,i) -= CoolingRate*dt*u_3;
         //Update internal energy
         if((r<10.1)&&(r>9.9)){
-          printf("Cooling On, Starg = %f, Rate = %.2e \n",s_targ,CoolingRate);
+          printf("Cooling On, Rate = %.2e \n",CoolingRate);
         }
         w0_(m,IEN,k,j,i) = Cooled_Energy;
       };
