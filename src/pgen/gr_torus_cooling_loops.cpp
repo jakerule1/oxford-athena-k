8f950ac901bb2feec26c6e342d52b68f083cf8a0
Jake Rule <rule@OXASWELAP13.physics.ox.ac.uk>
2025-02-26 16:39:24 -0500

    Add cooling rate print for polaris debug

--- src/pgen/gr_torus_cooling_loops.cpp
+++ src/pgen/gr_torus_cooling_loops.cpp
@@ -1992,8 +1992,8 @@ void Cooling(Mesh *pm, const Real dt) {
         // u0_(m,IM1,k,j,i) -= CoolingRate*dt*u_1;
         // u0_(m,IM2,k,j,i) -= CoolingRate*dt*u_2;
         // u0_(m,IM3,k,j,i) -= CoolingRate*dt*u_3;
-
         //Update internal energy
+        printf("Cooling On, Rate = %f \n",CoolingRate)
         w0_(m,IEN,k,j,i) = Cooled_Energy;
       };
     });
